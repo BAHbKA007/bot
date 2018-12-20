e1b7fcf1313e1fd44955c8e1eab1c94124b9e25e
Johann <peniskopfbacken@gmail.com>
2018-12-20 15:02:23 +0100

    asd

--- bot.py
+++ bot.py
@@ -227,7 +227,7 @@ def main(r):
                         # www_get(run, arc_count, 1, 1)
                         # print('Mehr als 6 Gegenstände 18+')
                         # break
-                        c = c - 36 * 6
+                        c = 0
                         k = k + 36
                     if arc_count == 12:
                         www_get(run, arc_count, 1, 1)
