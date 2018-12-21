479072e1be5485cbdd1730d4faf2fe75f2ef6a98
Johann <peniskopfbacken@gmail.com>
2018-12-21 14:15:21 +0100

    asd

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import time, pyautogui, sys, requests, urllib3, traceback, win32gui, os, datetime, re
+import time, pyautogui, sys, requests, urllib3, traceback, win32gui, os, datetime, re, math
 import pytesseract
 from interception import ffi, lib
 from PIL import Image
@@ -24,7 +24,7 @@ def mausklick():
 
 datum_sql = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 requests.get('http://s.leichtbewaff.net/?start='+str(datum_sql), verify=False)
-start_time = int(str(time.time())[0:10])
+start_time = int(math.ceil(time.time()))
 
 def main(r):
 
@@ -203,7 +203,7 @@ def main(r):
                 #     time.sleep(3)
 
                 # relog nach 30min
-                uhrzeit = int(str(time.time())[0:10])
+                uhrzeit = int(math.ceil(time.time()))
                 if (uhrzeit - start_time) % 1800 == 0 and (uhrzeit - start_time) != 0:
                     print('Relogg')
                     pyautogui.moveTo(relog)
--- tes.py
+++ tes.py
@@ -1,9 +1,9 @@
-import time, pyautogui, win32gui, os, re
+import time, pyautogui, win32gui, os, re, math
 from PIL import Image,ImageGrab
 
-start_time = int(str(time.time())[0:10])
+start_time = int(math.ceil(time.time()))
 
 while True:
-    uhrzeit = int(str(time.time())[0:10])
+    uhrzeit = int(math.ceil(time.time()))
     if (uhrzeit - start_time) % 5 == 0 and (uhrzeit - start_time) != 0:
         print('jetzt ' + str(uhrzeit - start_time))
