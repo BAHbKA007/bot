ef4018203890d8f890c24694e47e14a6fda0b62e
Johann <peniskopfbacken@gmail.com>
2018-12-23 01:28:38 +0100

    Process

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import time, pyautogui, sys, requests, urllib3, traceback, win32gui, os, datetime, re, math
-import pytesseract
+import pytesseract, subprocess, psutil
 from interception import ffi, lib
 from PIL import Image
 from skimage.io import imread
@@ -11,6 +11,39 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class SCANCODE:
     INTERCEPTION_MOUSE_LEFT_BUTTON_DOWN   = 0x001
     INTERCEPTION_MOUSE_LEFT_BUTTON_UP     = 0x002
+    TAB = 0x0F
+
+    anmeldedaten = {
+        'a' : 0x1E,
+        'b' : 0x30,
+        'h' : 0x23,
+        'k' : 0x25,
+        '0' : 0x0B,
+        '1' : 0x2,
+        '5' : 0x6,
+        '7' : 0x8,
+        '8' : 0x9,
+        '9' : 0x0A
+    }
+
+datum_sql = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+requests.get('http://s.leichtbewaff.net/?start='+str(datum_sql), verify=False)
+start_time = int(math.ceil(time.time()))
+
+def anmelden(benutzer,pw):
+    context = lib.interception_create_context()
+    kstroke = ffi.new('InterceptionKeyStroke *')
+
+    for key in benutzer:
+        kstroke.code = SCANCODE.anmeldedaten[key]
+        lib.interception_send(context, 1,  kstroke, 1)
+
+    kstroke.code = SCANCODE.TAB
+    lib.interception_send(context, 1,  kstroke, 1)
+
+    for key in pw:
+        kstroke.code = SCANCODE.anmeldedaten[key]
+        lib.interception_send(context, 1,  kstroke, 1)
 
 def mausklick():
     context = lib.interception_create_context()
@@ -21,17 +54,22 @@ def mausklick():
     lib.interception_send(context, 11, stroke, 1)
     stroke.state  = SCANCODE.INTERCEPTION_MOUSE_LEFT_BUTTON_UP
     lib.interception_send(context, 11,  stroke, 1)
+    lib.interception_destroy_context(context)
 
-datum_sql = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-requests.get('http://s.leichtbewaff.net/?start='+str(datum_sql), verify=False)
-start_time = int(math.ceil(time.time()))
+def login_versuche(start):
+    if time.time() - start > 120:
+        for proc in psutil.process_iter():
+            # check whether the process name matches
+            if proc.name() == 'l2.bin':
+                proc.kill()
+                time.sleep(2)
+                main(0)
 
 def main(r):
 
     path = str(os.path.dirname(__file__)) + '\\'
  
     e = 0
-    i = 5
     c = 0
     arc_count = 0
     v = 0.05 # 0.05 sleep auf den tasten
@@ -39,62 +77,24 @@ def main(r):
     k = 0
     while_count = 0
 
+
+
     #Windows Prozesse Nach lineage 2 durhsuchen
     wins = []
     proc = []
-    win32gui.EnumWindows(lambda x, y: y.append(x), wins)
-    for winId in wins:
-        winName = win32gui.GetWindowText(winId)
-        if winName == 'Lineage II':
-            proc.append(winId)
 
-    # Fenster in Fordergrund bringen
-    win32gui.SetForegroundWindow(proc[len(proc)-1])
+    def find_proc():
+        win32gui.EnumWindows(lambda x, y: y.append(x), wins)
+        for winId in wins:
+            winName = win32gui.GetWindowText(winId)
+            if winName == 'Lineage II':
+                proc.append(winId)
+        return proc
 
-    with open(path + "bot.run", "r") as fh:
-        run = int(fh.read())
-
-    while i != -1:
-        print(str(i))
-        time.sleep(1)
-        i = i - 1
-    i = 0
-    
-    if len(proc) > 0:
-        def logIn():
-            # Login: 462,443
-            find_pic('login.png')
-            mausklick()
-            time.sleep(4)
-
-            # Agree: 473,609
-            find_pic('agree.png')
-            mausklick()
-            time.sleep(6)
-
-            # OK: 517,450
-            find_pic('ok.png')
-            mausklick()
-            time.sleep(4)
-
-            # Start: 514,714
-            find_pic('start.png')
-            mausklick()
-            time.sleep(14)
-
-        win_pos = win32gui.GetWindowRect(proc[len(proc)-1])
-        win_pos_x = win_pos[0] + 7
-        win_pos_y = win_pos[1]
-
-        def find_pic(a, conf=.9, x=800, y=600, x_inner=0, y_inner=0):
-            pos = pyautogui.locateCenterOnScreen(path + 'pic\\' + a, region=(win_pos_x + x_inner, win_pos_y + y_inner,x,y),grayscale=True, confidence=conf)
-            pyautogui.moveTo(pos)
-            return pos      
-    else:
-        raise RuntimeError('Keine Lineage II Prozesse gefunden')
-
-    if find_pic('login.png') != None:
-        logIn()
+    def find_pic(a, conf=.9, x=800, y=600, x_inner=0, y_inner=0):
+        pos = pyautogui.locateCenterOnScreen(path + 'pic\\' + a, region=(win_pos_x + x_inner, win_pos_y + y_inner,x,y),grayscale=True, confidence=conf)
+        pyautogui.moveTo(pos)
+        return pos  
 
     def www_get(run, arc_count, discon, succes):
         requests.get('http://s.leichtbewaff.net/?run='+str(run)+'&arc='+str(arc_count)+'&discon='+str(discon) + '&succes=' + str(succes), verify=False)
@@ -118,20 +118,91 @@ def main(r):
 
         picture.save("ench_screen.png")
 
-        temp = pyautogui.locateAll(path + "pic\\14.png", "ench_screen.png", grayscale=False)
+        temp = pyautogui.locateAll(path + "pic\\18.png", "ench_screen.png", grayscale=False)
         i = len(list(temp))
 
         if arc_count >= i:
             return True
         else:
-            return False 
+            return False
 
-        if find_pic('login.png',.9,352,359,20,20) != None:
-            print('reLoginlog')
-            logIn()
-        else:
-            print('kein login on Screen')
-            pass
+    
+    def logIn():
+        start = int(math.ceil(time.time()))
+
+        while find_pic('login.png') == None:
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('login.png') != None:
+            # Login: 462,443
+            print('login Button')
+            find_pic('login.png')
+            mausklick()
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('agree.png') == None:
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('agree.png') != None:
+            # Agree: 473,609
+            print('agree Button')
+            find_pic('agree.png')
+            mausklick()
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('ok.png') == None:
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('ok.png') != None:
+            # OK: 517,450
+            print('OK Button')
+            find_pic('ok.png')
+            mausklick()
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('start.png') == None:
+            time.sleep(1)
+            login_versuche(start)
+
+        while find_pic('start.png') != None:
+            # Start: 514,714
+            print('Start Button')
+            find_pic('start.png')
+            mausklick()
+            time.sleep(1)
+            login_versuche(start)
+
+    if len(find_proc()) == 0:
+        p = subprocess.Popen([r"C:\\Users\\Johann\\Desktop\\Euro-PvP_Client_ru_en\\system\\l2.exe"], stdout=subprocess.PIPE)
+        p.wait()
+
+        while len(find_proc()) == 0:
+            time.sleep(1)
+
+        # Fenster in Fordergrund bringen
+        win32gui.SetForegroundWindow(find_proc()[len(find_proc())-1])
+
+        anmelden('bahbka1','090587')
+
+        logIn()
+
+    win32gui.SetForegroundWindow(find_proc()[len(find_proc())-1])
+    win_pos = win32gui.GetWindowRect(find_proc()[len(proc)-1])
+    win_pos_x = win_pos[0] + 7
+    win_pos_y = win_pos[1]
+
+    with open(path + "bot.run", "r") as fh:
+        run = int(fh.read())
+
+    while find_pic('ews.png') == None:
+        print('suche ews.png')
+        time.sleep(1)
 
     # ews Koordinaten, Farbe + Anzahl BEWS
     ews = find_pic('ews.png',0.99)
@@ -175,7 +246,7 @@ def main(r):
                     break
 
                 # alle 20 Durchläufe
-                if run % 20 == 0 and run != 0:
+                if ( start_time - int(math.ceil(time.time())) ) % 10 == 0:
                     print('Prüfungen: Disconnect und Spiel an')
                     #Prüfen ob Disconnect Fehlermeldung auf dem Bildschirm
                     if pyautogui.locateCenterOnScreen(path + 'pic\\disc.png', region=(win_pos_x + 258, win_pos_y + 270,40,40),grayscale=True, confidence=.9) != None:
@@ -213,8 +284,7 @@ def main(r):
                 #     time.sleep(3)
 
                 # relog nach 30min
-                uhrzeit = int(math.ceil(time.time()))
-                if (uhrzeit - start_time) % 1800 == 0 and (uhrzeit - start_time) != 0:
+                if ( start_time - int(math.ceil(time.time())) ) % 1800 == 0:
                     print('Relog')
                     pyautogui.moveTo(relog)
                     mausklick()
@@ -231,7 +301,7 @@ def main(r):
                 time.sleep(v)
 
                 # alle 100 Durchläufe
-                if run % 100 == 0 and run != 0:
+                if ( start_time - int(math.ceil(time.time())) ) % 300 == 0:
                     print('Reset Koordinaten Enchant window')
                     ench_window = pyautogui.locateOnScreen(path + 'pic\\enchantwindow.png', region=(win_pos_x, win_pos_y,800,600),grayscale=True, confidence=.9)
                     ench_window_x = ench_window[0]
--- tes.py
+++ tes.py
@@ -1,18 +1,19 @@
-import time, pyautogui, win32gui, os, re, math
+import time, pyautogui, win32gui, os, re, math, subprocess, psutil
 from PIL import Image,ImageGrab
+from interception import ffi, lib
 
-wins = []
-proc = []
-win32gui.EnumWindows(lambda x, y: y.append(x), wins)
-for winId in wins:
-    winName = win32gui.GetWindowText(winId)
-    if winName == 'Lineage II':
-        proc.append(winId)
+# wins = []
+# proc = []
+# win32gui.EnumWindows(lambda x, y: y.append(x), wins)
+# for winId in wins:
+#     winName = win32gui.GetWindowText(winId)
+#     if winName == 'Lineage II':
+#         proc.append(winId)
 
-path = str(os.path.dirname(__file__)) + '\\'
-win_pos = win32gui.GetWindowRect(proc[len(proc)-1])
-win_pos_x = win_pos[0] + 7
-win_pos_y = win_pos[1]
+# path = str(os.path.dirname(__file__)) + '\\'
+# win_pos = win32gui.GetWindowRect(proc[len(proc)-1])
+# win_pos_x = win_pos[0] + 7
+# win_pos_y = win_pos[1]
 
 # ench_window = pyautogui.locateOnScreen(path + 'pic\\enchantwindow.png', region=(win_pos_x, win_pos_y,800,600),grayscale=True, confidence=.9)
 # ench_window_x = ench_window[0]
@@ -23,9 +24,62 @@ win_pos_y = win_pos[1]
 # while pyautogui.locateOnScreen(path + 'pic\\cancel.png', region=(ench_window_x + 160, ench_window_y + 382, 6, 8),grayscale=True, confidence=.99) != None:
 #     print('da')
 
-def find_pic(a, conf=.9, x=800, y=600, x_inner=0, y_inner=0):
-    pos = pyautogui.locateCenterOnScreen(path + 'pic\\' + a, region=(win_pos_x + x_inner, win_pos_y + y_inner,x,y),grayscale=True, confidence=conf)
-    pyautogui.moveTo(pos)
-    return pos
+class SCANCODE:
+    INTERCEPTION_MOUSE_LEFT_BUTTON_DOWN   = 0x001
+    INTERCEPTION_MOUSE_LEFT_BUTTON_UP     = 0x002
+    TAB = 0x0F
 
-print(find_pic('login.png',.9,352,359,20,20))
+    anmeldedaten = {
+        'a' : 0x1E,
+        'b' : 0x30,
+        'h' : 0x23,
+        'k' : 0x25,
+        '0' : 0x0B,
+        '1' : 0x2,
+        '5' : 0x6,
+        '7' : 0x8,
+        '8' : 0x9,
+        '9' : 0x0A
+    }
+
+def anmelden(benutzer,pw):
+    context = lib.interception_create_context()
+    kstroke = ffi.new('InterceptionKeyStroke *')
+
+    for key in benutzer:
+        kstroke.code = SCANCODE.anmeldedaten[key]
+        lib.interception_send(context, 1,  kstroke, 1)
+
+    kstroke.code = SCANCODE.TAB
+    lib.interception_send(context, 1,  kstroke, 1)
+
+    for key in pw:
+        kstroke.code = SCANCODE.anmeldedaten[key]
+        lib.interception_send(context, 1,  kstroke, 1)
+
+#Windows Prozesse Nach lineage 2 durhsuchen
+wins = []
+proc = []
+
+def find_proc():
+    win32gui.EnumWindows(lambda x, y: y.append(x), wins)
+    for winId in wins:
+        winName = win32gui.GetWindowText(winId)
+        if winName == 'Lineage II':
+            proc.append(winId)
+    return proc
+
+
+if find_proc() == 0:
+    p = subprocess.Popen([r"C:\\Users\\Johann\\Desktop\\Euro-PvP_Client_ru_en\\system\\l2.exe"], stdout=subprocess.PIPE)
+    p.wait()
+    time.sleep(15)
+
+    anmelden('bahbka1','090587')
+
+# Fenster in Fordergrund bringen
+#win32gui.SetForegroundWindow(proc[len(proc)-1])
+for proc in psutil.process_iter():
+    # check whether the process name matches
+    if proc.name() == 'l2.bin':
+        proc.kill()
