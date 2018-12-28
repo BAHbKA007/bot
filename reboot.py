852162857c2f8193058230a8e7c3967907fe90af
Johann <peniskopfbacken@gmail.com>
2018-12-28 01:55:52 +0100

    46

--- reboot.py
+++ reboot.py
@@ -1,4 +1,6 @@
-import os, time
+import os, time, requests, urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 neustart = 3600
 start_time = time.time()
 
@@ -8,12 +10,12 @@ while True:
     m = str(int(rest / 60 % 60))
     s = str(rest % 60 % 60)
 
-    printstring = ' | Neustart in: Sek: ' + str(rest) + ' | in Std: ' + h.zfill(2) + ' : ' + m.zfill(2) + ' : ' + s.zfill(2)
+    printstring = 'Neustart in: Sek: ' + str(rest) + ' | in Std: ' + h.zfill(2) + ' : ' + m.zfill(2) + ' : ' + s.zfill(2)
     print(printstring, end='')
     print('\b' * len(printstring), end='', flush=True)
 
     if (start_time + neustart - time.time()) < 0:
         print('Reboot PC...')
         os.system("shutdown -t 0 -r -f")
-
+    requests.get('http://s.leichtbewaff.net/?reboot=' + printstring, verify=False)
     time.sleep(1)
