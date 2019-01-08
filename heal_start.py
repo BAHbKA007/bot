488b978342375fd5965deacbd5b0418038719b8b
Johann <peniskopfbacken@gmail.com>
2019-01-08 09:13:15 +0100

    asd

--- heal_start.py
+++ heal_start.py
@@ -5,7 +5,7 @@ while True:
     zeit = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
     if win32gui.FindWindow(None, "Administrator:  Heal") == 0:
         print(zeit + ' | Starte Bot')
-        subprocess.call("start /wait python " + path + "bot.py" , shell=True)
+        subprocess.call("start /wait python " + path + "heal.py" , shell=True)
     else:
         continue
 
